aad78bcbf3e320938615cd2bbaf03959627ccb3c
kylefarmer85 <kylefarmer85@gmail.com>
2020-09-27 21:27:46 -0500

    Pre migration

---
+++ db/002_create_listing.rb
@@ -0,0 +1,12 @@
+class CreateListing < ActiveRecord::Migration[5.2]
+  def change
+    create_table :listings do |t|
+      t.string :title
+      t.string :description
+      t.string :address
+      t.string :listing_type
+      t.integer :price
+      t.integer :host_id
+    end
+  end
+end
---
+++ db/003_create_neighborhood.rb
@@ -0,0 +1,7 @@
+class CreateNeighborhood < ActiveRecord::Migration[5.2]
+  def change
+    create_table :neighborhoods do |t|
+      t.string :name
+    end
+  end
+end
